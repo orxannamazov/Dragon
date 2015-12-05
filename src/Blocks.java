5802927e6ef5223d23f66676e8f453e6698d0804
rails --help <guraybaydur@gmail.com>
2015-12-05 17:15:20 +0200

    added block

---
+++ src/Blocks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Güray
+ */
+public class Blocks 
+{
+    ArrayList<Block> blocks;
+    
+    public void createBlocks(Block newBlock){
+        blocks.add(newBlock);
+    }
+    
+    public void drawBlocks(BufferedImage image)
+    {
+        
+    }
+}
